74e7eeaeaa50bc68f7334f137a9fd344e60f197b
Andrew Toulouse <andrew@atoulou.se>
2014-07-07 23:53:40 -0700

    Initial Commit

---
+++ lllib/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  lllib
+//
+//  Created by Andrew Toulouse on 7/7/14.
+//  Copyright (c) 2014 Andrew Toulouse. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
