55d38e4cd3e99d6bf86b44c1ea1ae38c5f08949c
Luke Granger-Brown <git@lukegb.com>
2014-08-15 21:09:03 -0400

    Tidies up a little
    * libOL_test now does some basic error handling.
    * Removes stray .gitignore
    * Wraps zlib include in extern "C" declaration

--- src/libOL_test/main.cpp
+++ src/libOL_test/main.cpp
@@ -3,37 +3,83 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <cassert>
+#include <cstring>
 
 #include <libOL/Chunks.h>
 #include <libOL/Rofl.h>
 #include <libOL/Keyframe.h>
 
-void test_keyframe()
-{
+#define MAX_ARGUMENT_LENGTH 600
 
-    std::ifstream ifs("../../lol-spec/analysis2/keyframes/EUW1.1617523548.1.keyframe", std::ios::binary);
+int test_keyframe(std::vector<std::string> arguments)
+{
+    assert(arguments.size() == 1);
+
+    std::ifstream ifs(arguments.at(0), std::ios::binary);
+    if (!ifs) {
+        std::cerr << "Failed to open " << arguments.at(0) << ": " << strerror(errno) << std::endl;
+	return 2;
+    }
 
     libol::Keyframe frame = libol::Keyframe::decode(ifs);
 
     for(auto& player : frame.players) {
         std::cout << player.summonerName << " as " << player.championName << std::endl;
     }
+
+    return 0;
 }
 
 
-void test_rofl() {
-    std::ifstream ifs("/Users/toulouse/code/lol/lmao/rofl/22923174.rofl", std::ios::binary);
+int test_rofl(std::vector<std::string> arguments)
+{
+    assert(arguments.size() == 1);
+
+    std::ifstream ifs(arguments.at(0), std::ios::binary);
+    if (!ifs) {
+        std::cerr << "Failed to open " << arguments.at(0) << ": " << strerror(errno) << std::endl;
+	return 2;
+    }
 
     libol::Rofl rofl = libol::Rofl::decode(ifs);
 
     auto header0 = rofl.chunkHeaders[0];
     auto chunk = rofl.getDecryptedChunk(ifs, header0);
+
+    return 0;
+}
+
+int usage(std::string prog_name) {
+    std::cerr << prog_name << " [rofl|keyframe] <rofl/keyframe file>" << std::endl;
+    return 1;
 }
 
 int main(int argc, const char * argv[])
 {
-    test_keyframe();
-    return 0;
+    std::string executable_name = std::string(argv[0], 0, MAX_ARGUMENT_LENGTH);
+    if (argc < 3) {
+        return usage(executable_name);
+    }
+
+    // build argument vector
+    std::vector<std::string> arguments;
+    for (int i = 1; i < argc; ++i) {
+        arguments.push_back(std::string(argv[i], 0, MAX_ARGUMENT_LENGTH));
+    }
+
+    std::string command = arguments.at(0);
+    arguments.erase(arguments.begin());
+
+    if (command == "rofl") {
+        return test_rofl(arguments);
+    } else if (command == "keyframe") {
+        return test_keyframe(arguments);
+    }
+
+    return usage(executable_name);
 }
 
 
