70c7bea4642f19ac4ca565556d0568c5f1669c6e
satan6 <satan6@users.noreply.github.com>
2014-09-17 01:28:53 +0200

    Put Value in the namespace

--- src/libOL/Value.h
+++ src/libOL/Value.h
@@ -9,85 +9,102 @@
 #include <vector>
 #include <cstdint>
 
-class Object;
-class Array;
+namespace libol {
+    class Object;
+    class Array;
 
-class Value {
-public:
-    enum {
-        UNDEFINED,
-        OBJECT, // Object
-        ARRAY, // Array
-        STRING, // std::string
-        INTEGER, // int32_t
-        LARGE_INTEGER, // int64_t
-        FLOAT, // float
-        BOOL // bool
-    } type;
-    void* value;
+    class Value {
+    public:
+        enum {
+            UNDEFINED,
+            OBJECT, // Object
+            ARRAY, // Array
+            STRING, // std::string
+            INTEGER, // int32_t
+            LARGE_INTEGER, // int64_t
+            FLOAT, // float
+            BOOL // bool
+        } type;
+        void *value;
 
-    Value() : type(UNDEFINED), value(nullptr) {}
-    void destroy();
+        Value() : type(UNDEFINED), value(nullptr) {
+        }
 
-    template<class T>
-    T& as() {
-        return *reinterpret_cast<T*>(value);
-    }
+        void destroy();
 
-    std::string toString(size_t indent = 0);
+        template<class T>
+        T &as() {
+            return *reinterpret_cast<T *>(value);
+        }
 
-    static Value create(Object& val);
-    static Value create(Array& val);
-    static Value create(std::string& val);
-    static Value create(const char*& val);
-    static Value create(uint64_t& val);
-    static Value create(uint32_t& val);
-    static Value create(uint16_t& val);
-    static Value create(uint8_t& val);
-    static Value create(int64_t& val);
-    static Value create(int32_t& val);
-    static Value create(int16_t& val);
-    static Value create(int8_t& val);
-    static Value create(float& val);
-    static Value create(bool& val);
-};
+        std::string toString(size_t indent = 0);
 
-class Object {
-    std::map<std::string, Value> map;
-public:
-    template<class T>
-    void setv(std::string name, T value) {
-        set(name, Value::create(value));
-    }
+        static Value create(Object &val);
 
-    void set(std::string name, Value value);
+        static Value create(Array &val);
 
-    Value get(std::string name);
+        static Value create(std::string &val);
 
-    size_t size();
+        static Value create(const char *&val);
 
-    std::map<std::string, Value>::iterator begin() {
-        return map.begin();
-    }
+        static Value create(uint64_t &val);
 
-    std::map<std::string, Value>::iterator end() {
-        return map.end();
-    }
-};
+        static Value create(uint32_t &val);
 
-class Array {
-    std::vector<Value> vector;
-public:
-    template<class T>
-    void pushv(T value) {
-        push(Value::create(value));
+        static Value create(uint16_t &val);
+
+        static Value create(uint8_t &val);
+
+        static Value create(int64_t &val);
+
+        static Value create(int32_t &val);
+
+        static Value create(int16_t &val);
+
+        static Value create(int8_t &val);
+
+        static Value create(float &val);
+
+        static Value create(bool &val);
     };
 
-    void push(Value value);
+    class Object {
+        std::map<std::string, Value> map;
+    public:
+        template<class T>
+        void setv(std::string name, T value) {
+            set(name, Value::create(value));
+        }
 
-    size_t size();
+        void set(std::string name, Value value);
 
-    Value at(size_t index);
-};
+        Value get(std::string name);
+
+        size_t size();
+
+        std::map<std::string, Value>::iterator begin() {
+            return map.begin();
+        }
+
+        std::map<std::string, Value>::iterator end() {
+            return map.end();
+        }
+    };
+
+    class Array {
+        std::vector<Value> vector;
+    public:
+        template<class T>
+        void pushv(T value) {
+            push(Value::create(value));
+        };
+
+        void push(Value value);
+
+        size_t size();
+
+        Value at(size_t index);
+    };
+}
 
 #endif /* defined(__libol__Value__) */
